d6c4ebda40f3fa702017bb91a8b7ee49ae0953ca
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-04 14:09:31 +0530

    Day 3

--- problem04.py
+++ problem04.py
@@ -8,3 +8,4 @@ for i in range(1,n+1):
         print(" "*(n-2),end="")
         print("*", end="")
     print("")
+
---
+++ problem05.py
@@ -0,0 +1,7 @@
+n = int(input())
+l1 = []
+for i in range(1,11):
+    a = n*i
+    l1.append(a)
+    i += 1
+print(l1)

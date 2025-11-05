a2f2a4a3b3ff5e067207c8a340f0f9ef558049ee
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-05 21:34:34 +0530

    Day 4

---
+++ problem08.py
@@ -0,0 +1,12 @@
+words = {"keys": "values"}
+# words.keys("jal" , "raat" ,"subah" , "soona")
+# words.values("water" , "night" ,"morning" , "sleeping")
+# print(words)
+
+words ={
+    "billi" : "cat" ,
+    "jal" : "water" ,
+    "raat":"night"
+    }
+word = input("Enter the word u want to know meaning of:")
+print(words[word])

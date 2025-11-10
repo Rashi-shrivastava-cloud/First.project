51e830f94b4ff8d171c4058bc4d23eb11b0de1c9
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-10 21:49:05 +0530

    Day 5

--- problem13.py
+++ problem13.py
@@ -1,8 +1,38 @@
-l1 = [12 , 23 , 45 , 67 ]
-# for i in range(len(l1)):
-#     print(l1[i] , end = " ")
-sum = (l1[0] + l1[1] + l1[2] +l1[3])
-print(sum)
+# l1 = [12 , 23 , 45 , 67 ]
+# # for i in range(len(l1)):
+# #     print(l1[i] , end = " ")
+# sum = (l1[0] + l1[1] + l1[2] +l1[3])
+# print(sum)
+#
+# # another way
+# print(sum(l1))
 
-# another way
-print(sum(l1))
+
+n = int(input("enter the number:"))
+if (n<1 or n>9):
+    print("Invalid Input")
+
+else:
+    choice = input()
+
+    if (choice == "n"):
+        print(n)
+
+    elif(choice == "y"):
+        operation = input()
+        n2 = int(input("enter the another number:"))
+
+        if (operation == "+"):
+            result = n + n2
+            print(result)
+
+        elif(operation == "-"):
+            result = n - n2
+            print(result)
+
+        elif(operation == "*"):
+            result = n * n2
+            print(result)
+
+        else:
+            print("Invalid operation .")

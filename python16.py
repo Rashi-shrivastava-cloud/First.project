406dbeaf046e4278ca430bb05c3d684630b78e8a
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-10 16:48:07 +0530

    Day 5

--- python16.py
+++ python16.py
@@ -1,4 +1,11 @@
+# n = int(input())
+# l1 = list(map(int,input().split()))
+# l1.sort()
+# print(l1[0])
+
+
+
 n = int(input())
 l1 = list(map(int,input().split()))
-l1.sort()
-print(l1[0])
+l1.sort(reverse = True)
+print(l1)

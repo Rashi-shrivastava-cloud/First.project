718b158a2dbb01b4397bdb90b3b01a6af0e76fa9
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-10 18:36:02 +0530

    Day 5

--- python16.py
+++ python16.py
@@ -5,7 +5,28 @@
 
 
 
-n = int(input())
-l1 = list(map(int,input().split()))
-l1.sort(reverse = True)
-print(l1)
+# n = int(input())
+# l1 = list(map(int,input().split()))
+# l1.sort(reverse = True)
+# print(l1)
+
+
+def fun(num):
+    num_set =set(num)
+    longest = []
+
+    for i in (num_set):
+        if (i - 1 not in num_set):
+            current = []
+            x = i
+        while (x in num_set):
+             current.append(x)
+             x += 1
+
+        if len(current) > len(longest):
+            longest = current
+    return tuple(longest)
+
+t = tuple(map(int,input().split()))
+arr= fun(t)
+print(arr)

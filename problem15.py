f0609816121c2aff154f55e8fe7a167c9323d5c5
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-10 20:51:27 +0530

    Day 5

--- problem15.py
+++ problem15.py
@@ -1,8 +1,24 @@
-n = int(input())
-l1 = list(map(str,input().split()))
-m = int(input())
-l2 = list(map(str,input().split()))
-s1 = set(l1)
-s2 = set(l2)
-difference = s1.difference(s2)
-print(difference)
+# n = int(input())
+# l1 = list(map(str,input().split()))
+# m = int(input())
+# l2 = list(map(str,input().split()))
+# s1 = set(l1)
+# s2 = set(l2)
+# difference = s1.difference(s2)
+# print(difference)
+#
+
+
+str1 = input()
+str2 = input()
+str3 = str1 + str2
+print(str3)
+vowels = "aeiouAEIOU"
+for ch in vowels:
+    str3 = str3.replace(ch , "")
+print(str3)
+
+str4 = str3[::-1]
+print(str4)
+str5 = str4.swapcase()
+print(str5)

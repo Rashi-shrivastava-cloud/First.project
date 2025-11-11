f6889450c404ca29f4c598197ea96b7c0ea520d8
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-11 21:10:39 +0530

    Day 5

--- problem18.py
+++ problem18.py
@@ -1,9 +1,14 @@
-user_input= input().split()
-for i in (user_input):
-    if i.isalpha():
-        print(f"yes it is an alphabet{i}")
-    elif i.isdigit():
-        print(f"yes it is a digit{i}")
+# user_input= input().split()
+# for i in (user_input):
+#     if i.isalpha():
+#         print(f"yes it is an alphabet{i}")
+#     elif i.isdigit():
+#         print(f"yes it is a digit{i}")
+#
+#     else:
+#         print("invalid input")
 
-    else:
-        print("invalid input")
+
+l1 = list(map(int,input().split()))
+print(max(l1))
+print(min(l1))

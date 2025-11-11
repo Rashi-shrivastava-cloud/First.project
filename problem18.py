f80b9589e833952e5a53d3b7113b655b82dd5b48
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-11 21:51:31 +0530

    Day 5

--- problem18.py
+++ problem18.py
@@ -9,6 +9,19 @@
 #         print("invalid input")
 
 
-l1 = list(map(int,input().split()))
-print(max(l1))
-print(min(l1))
+# l1 = list(map(int,input().split()))
+# print(max(l1))
+# print(min(l1))
+
+# n = int(input())
+# l1 =list(map(int,input().split()))
+# rotated_list = l1[-2:] + l1[:-2]
+# print(rotated_list)
+
+
+a = input()
+a2 =a.upper()
+print(a2)
+
+a3 = a2.lower()
+print(a3)

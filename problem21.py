792812e53652a8c40fa9a5e72ba72465f8ad5c65
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-16 23:33:56 +0530

    Day 5

--- problem21.py
+++ problem21.py
@@ -6,3 +6,11 @@ if (name in l1):
 
 else:
     print("no it is not present in the list " , name)
+
+l1 = ["rashi" , "anumita" , "vanshu" , "ram"]
+name = input("Enter your name:")
+if (name in l1):
+    print("yes name is present in the list" , name)
+
+else:
+    print("no it is not present in the list " , name)

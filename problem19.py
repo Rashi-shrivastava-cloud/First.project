780e2843957918c6f087a659df088f3bcda4dcdd
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-16 23:24:42 +0530

    Day 5

--- problem19.py
+++ problem19.py
@@ -6,3 +6,12 @@ for i in a:
         count[i] +=1
 
 print(count)
+
+
+l1 = ["rashi" , "anumita" , "vanshu" , "ram"]
+name = input("Enter your name:")
+if (name in l1):
+    print("yes name is present in the list" , name)
+
+else:
+    print("no it is not present in the list " , name)

1236071e60aab179c5f4e317d0b35b8b938d021b
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-22 22:31:02 +0530

    Day 5

--- problem15.py
+++ problem15.py
@@ -8,17 +8,24 @@
 # print(difference)
 #
 
+#
+# str1 = input()
+# str2 = input()
+# str3 = str1 + str2
+# print(str3)
+# vowels = "aeiouAEIOU"
+# for ch in vowels:
+#     str3 = str3.replace(ch , "")
+# print(str3)
+#
+# str4 = str3[::-1]
+# print(str4)
+# str5 = str4.swapcase()
+# print(str5)
 
-str1 = input()
-str2 = input()
-str3 = str1 + str2
-print(str3)
-vowels = "aeiouAEIOU"
-for ch in vowels:
-    str3 = str3.replace(ch , "")
-print(str3)
 
-str4 = str3[::-1]
-print(str4)
-str5 = str4.swapcase()
-print(str5)
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end = "")

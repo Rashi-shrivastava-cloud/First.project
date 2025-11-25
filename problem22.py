43c26a50c2da654897763f2f5a58b063f5cc1f43
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-25 19:58:39 +0530

    Day 5

--- problem22.py
+++ problem22.py
@@ -1,11 +1,17 @@
-x = int(input())
-y = int(input())
-z = int(input())
+# x = int(input())
+# y = int(input())
+# z = int(input())
+# n = int(input())
+# 
+# coordinates = [[i,j,k]
+# for i in range(x+1)
+# for j in range(y+1)
+# for k in range(z+1)]
+# 
+# print(coordinates)
+
 n = int(input())
-
-coordinates = [[i,j,k]
-for i in range(x+1)
-for j in range(y+1)
-for k in range(z+1)]
-
-print(coordinates)
+arr = list(map(int,input().split()))
+a = list(set(arr))
+a.sort(reverse = True)
+print(a[1])

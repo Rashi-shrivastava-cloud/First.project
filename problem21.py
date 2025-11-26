d8233c6414885e20f52005716111993f00727fc0
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-26 23:37:54 +0530

    Day 5

--- problem21.py
+++ problem21.py
@@ -1,25 +1,20 @@
 
-l1 = ["rashi" , "anumita" , "vanshu" , "ram"]
-name = input("Enter your name:")
-if (name in l1):
-    print("yes name is present in the list" , name)
+# l1 = ["rashi" , "anumita" , "vanshu" , "ram"]
+# name = input("Enter your name:")
+# if (name in l1):
+#     print("yes name is present in the list" , name)
+#
+# else:
+#     print("no it is not present in the list " , name)
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
 
-else:
-    print("no it is not present in the list " , name)
 
-l1 = ["rashi" , "anumita" , "vanshu" , "ram"]
-name = input("Enter your name:")
-if (name in l1):
-    print("yes name is present in the list" , name)
-
-else:
-    print("no it is not present in the list " , name)
-
-
-l1 = ["rashi" , "anumita" , "vanshu" , "ram"]
-name = input("Enter your name:")
-if (name in l1):
-    print("yes name is present in the list" , name)
-
-else:
-    print("no it is not present in the list " , name)
+avg_marks = sum(student_marks[query_name])/3
+print("{:.2f}".format(avg_marks))

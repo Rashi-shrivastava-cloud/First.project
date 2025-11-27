5bae53245a68d2794232be5853ac5644830d70ad
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-27 22:06:37 +0530

    Day 5

--- problem21.py
+++ problem21.py
@@ -6,15 +6,24 @@
 #
 # else:
 #     print("no it is not present in the list " , name)
-if __name__ == '__main__':
-    n = int(input())
-    student_marks = {}
-    for _ in range(n):
-        name, *line = input().split()
-        scores = list(map(float, line))
-        student_marks[name] = scores
-    query_name = input()
+
+#     n = int(input())
+#     student_marks = {}
+#     for _ in range(n):
+#         name, *line = input().split()
+#         scores = list(map(float, line))
+#         student_marks[name] = scores
+#     query_name = input()
+#
+#
+# avg_marks = sum(student_marks[query_name])/3
+# print("{:.2f}".format(avg_marks))
+
+# # m = []
+# m = [i for i in range(11)]
+# print(m)
 
 
-avg_marks = sum(student_marks[query_name])/3
-print("{:.2f}".format(avg_marks))
+l1 = ["rashi" ,"anas" , "raj" ,"anumita"]
+l2 = [i.upper() for i in l1]
+print(l2)

36c2a06535bfdaef1ac06eae02cc5bdd8b96c2d6
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-11-29 22:42:02 +0530

    Day 5

---
+++ problem23.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    # N = int(input())
+    N = int(input())
+l1= []
+l1.insert(0,5)
+l1.insert(1,10)
+l1.insert(0,6)
+print(l1)
+l1.remove(6)
+l1.insert(0,9)
+l1.append(1)
+
+l1.sort()
+print(l1)
+l1.sort(reverse = True)
+print(l1)
+l1.pop(3)
+print(l1)
+l1.sort(reverse = True)
+print(l1)

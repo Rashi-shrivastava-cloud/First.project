34225942794fe67ba403a0900c7befb8f60f2d3b
Rashi-shrivastava-cloud <rashudelhi25@gmail.com>
2025-12-09 21:49:26 +0530

    Day 5

--- problem24.py
+++ problem24.py
@@ -1,22 +1,36 @@
-n = int(input())
-student = []
-for i in range(n):
-    name = input()
-    grades = float(input())
-    student.append([name,grades])
-
-print(student)
-grades =[ student[1] for  student in student ]
-unique_grade = sorted(set(grades))
-print(unique_grade)
-
-
-second_lowest = grades[1]
-print(second_lowest)
-
-name = [student[0] for student in student if student[1] == second_lowest]
-name.sort()
-print("/n" .join(name))
-
+# n = int(input())
+# student = []
+# for i in range(n):
+#     name = input()
+#     grades = float(input())
+#     student.append([name,grades])
+#
+# print(student)
+# grades =[ student[1] for  student in student ]
+# unique_grade = sorted(set(grades))
+# print(unique_grade)
+#
+#
+# second_lowest = grades[1]
+# print(second_lowest)
+#
+# name = [student[0] for student in student if student[1] == second_lowest]
+# name.sort()
+#
 # for a in name:
 #     print(a)
+
+
+# include <stdio.h>
+
+int main()
+{
+int a, b;
+    printf("enter the number(a)");
+    scanf("%d", & a);
+    printf("enter the number(b)");
+    scanf("%d", & b);
+    printf("%d", a + b);
+
+    return 0;
+}
